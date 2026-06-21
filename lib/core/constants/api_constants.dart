e9f0741d87ad072c224a591446fc7c3321354d99
irina <132u@mail.ru>
2026-06-21 16:10:07 +0300

    fixed ApiConstants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -15,17 +15,17 @@ import 'package:flutter/foundation.dart';
 // }
 class ApiConstants {
   static String get baseUrl {
-    if (kIsWeb) {
-      return '/api';
-    }
+    // if (kIsWeb) {
+    //   return '/api';
+    // }
 
     return 'http://manibase.ru/api';
   }
 
   static String get serverUrl {
-    if (kIsWeb) {
-      return '';
-    }
+    // if (kIsWeb) {
+    //   return '';
+    // }
 
     return 'http://manibase.ru';
   }
