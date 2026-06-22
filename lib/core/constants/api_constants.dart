f1aea0d8aa6d548a3e2e1529c19f86d65893e2d0
irina <132u@mail.ru>
2026-06-22 10:20:43 +0300

    fixed ApiConstants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/foundation.dart';
-// class ApiConstants {
-//   static String get baseUrl {
-//     return 'http://localhost:5288/api';
-//   }
-
-//   static String get serverUrl {
-//     return 'http://localhost:5288';
-//   }
-
-//   static String imageUrl(String path) {
-//     if (path.startsWith('http')) return path;
-//     return '$serverUrl$path';
-//   }
-// }
 class ApiConstants {
   static String get baseUrl {
-    // if (kIsWeb) {
-    //   return '/api';
-    // }
+    if (kIsWeb) {
+      return '/api';
+    }
 
     return 'http://manibase.ru/api';
   }
 
   static String get serverUrl {
-    // if (kIsWeb) {
-    //   return '';
-    // }
+    if (kIsWeb) {
+      return '';
+    }
 
-    return 'http://manibase.ru';
+    return 'http://manibase.ru/api';
   }
 
   static String imageUrl(String path) {
