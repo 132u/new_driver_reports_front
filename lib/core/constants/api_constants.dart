1ae1738e38450fb3ef8983373f2d4171c24f863a
irina <132u@mail.ru>
2026-06-27 09:07:08 +0300

    auto deploy

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/foundation.dart';
 class ApiConstants {
-  static String get baseUrl {
-    if (kIsWeb) {
-      return '/api';
-    }
-
-    return 'http://manibase.ru/api';
-  }
-
-  static String get serverUrl {
-    if (kIsWeb) {
-      return '';
-    }
-
-    return 'http://manibase.ru/api';
-  }
+  static const serverUrl = 'https://manibase.ru';
+  static const baseUrl = '$serverUrl/api';
 
   static String imageUrl(String path) {
     if (path.startsWith('http')) return path;
     return '$serverUrl$path';
   }
-  
 }
--- lib/core/constants/app_version.dart
+++ lib/core/constants/app_version.dart
@@ -2,6 +2,6 @@ import 'package:flutter/foundation.dart';
 
 class AppVersion {
   static String get appVersion {
-    return '3.1.0';
+    return '4.1.0';
   }
 }
